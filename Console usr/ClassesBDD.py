604df36aeb6885815bf834dd51f44e2c5a6a0255
Nexus-Strife <34130887+Nexus-Strife@users.noreply.github.com>
2018-07-20 14:51:55 +0200

    Assainissement
    Assainissement du code, ajouts de commentaires et amélioration de la note pylint.

--- Console usr/ClassesBDD.py
+++ Console usr/ClassesBDD.py
@@ -1,6 +1,7 @@
-import mysql.connector
-# from mysql.connector import errorcode
 import time
+import mysql.connector
+
+
 
 config = {
     'host': '172.18.0.1',
@@ -36,7 +37,9 @@ class Console:  # Main class of the program
 
                 id_sel = int(input("\nVeuillez selectionner une catégorie ou retourner au menu principal (99): "))
 
-                cursor.execute("SELECT id FROM categories WHERE id = %s", (id_sel, ))  # This SQL req check if the id of the category entered before exist
+                cursor.execute("SELECT id FROM categories WHERE id = %s", (id_sel, ))
+                # This SQL req check if the id of the category entered before exist
+
                 id_verified = cursor.fetchone()
 
                 if id_sel == 99:
@@ -52,7 +55,8 @@ class Console:  # Main class of the program
                     for row in cursor:
                         print("Produit: {name}, Note: {grade}, ID: {prod_id}, Lien: {link}".format(**row))
 
-            except ValueError:  # End of this first try...except block. If there are str in 'id_sel' than reply "You can't use chars"
+            except ValueError:
+                # End of this first try...except block. If there are str in 'id_sel' than reply "You can't use chars"
                 print("\nQ.U.E   D.E.S   C.H.I.F.F.R.E.S")
 
         another_menu = 1
@@ -81,29 +85,32 @@ class Console:  # Main class of the program
 
                                 product_id = int(input("\nVeuillez entrer l'ID"
                                                        " du produit que vous souhaitez enregistrer: "))
-                                cursor.execute("SELECT * FROM products WHERE prod_id = %s", (product_id, ))  # This cursor.execute check if the product_id exist in the database
+                                cursor.execute("SELECT * FROM products WHERE prod_id = %s", (product_id, ))
+                                # This cursor.execute check if the product_id exist in the database
+
                                 id_exist = cursor.fetchone()
 
                                 if not id_exist:
-                                    print("\nVotre produit n'est pas dans la base de données")  # If not exist, notify it to the user
+                                    print("\nVotre produit n'est pas dans la base de données")
+                                    # If not exist, notify it to the user
 
-                                else:
+                                else:  # Otherwise register it into the database
                                     print("Enregistrement en cours...")
-                                    cursor.execute("INSERT INTO usr_products (prod_id) VALUES (%s)"  # Otherwise register it into the database
+                                    cursor.execute("INSERT INTO usr_products (prod_id) VALUES (%s)"  
                                                    " ON DUPLICATE KEY UPDATE prod_id = prod_id", (product_id, ))
+
                                     cnx.commit()
                                     time.sleep(0.2)
                                     ctrl_nbr_products = 0
 
                                 try:  # Another one here to check the value of the next var ( 'go_again' )
                                     go_again = int(input("\nVoulez-vous ajouter un autre produit ?"
-                                                        " \nOui (1)\nNon (2)\nUn substitut (3)"))
+                                                         " \nOui (1)\nNon (2)\nUn substitut (3)"))
 
                                     if go_again == 1:
                                         continue
 
                                     elif go_again == 2:
-                                        another = 0
                                         self.menu()
 
                                     elif go_again > 3:
@@ -112,7 +119,12 @@ class Console:  # Main class of the program
                                     elif go_again < 1:
                                         print("Veuillez choisir un chiffre entre 1 et 3 !")
 
-                                    elif go_again == 3:  # The three next cursor.execute put in var the category ID, the grade and the energy per 100g that going to compose the search args to find a substitute
+                                    elif go_again == 3:
+
+                                        ''' The three next cursor.execute put in var the category ID, 
+                                            the grade and the energy per 100g that going to compose the search args
+                                             to find a substitute '''
+
                                         cursor.execute("SELECT cat_id FROM products WHERE prod_id = %(a)s",
                                                        {'a': product_id})
                                         cat_prod_fetch = cursor.fetchone()
@@ -128,22 +140,26 @@ class Console:  # Main class of the program
                                         nrg = cursor.fetchone()
                                         enrgy_prod = nrg['kcal_100g']
 
-                                        cursor.execute("SELECT * FROM products"
-                                                  " WHERE cat_id = %s AND grade <= %s AND kcal_100g <= %s",(cat_prod_id,
-                                                                                                            grade_prod,
-                                                                                                            enrgy_prod, ))  # SQl req to search a substitute based on the criteria mentioned previously
-                                        for row in cursor:
+                                        # SQl req to search a substitute based on the criteria mentioned previously
+                                        cursor.execute("SELECT * FROM products WHERE"
+                                                       " cat_id = %s AND grade <= %s AND kcal_100g <= %s",
+                                                       (cat_prod_id, grade_prod, enrgy_prod, ))
+
+                                        for row in cursor:  # Then print the results
                                             print("\nID: {prod_id},\nNom: {name},\nDesc: {description},\n"
-                                                  "Note: {grade},\nEnergie: {kcal_100g},\nLien: {link}\n".format(**row))  # Then print the results
+                                                  "Note: {grade},\nEnergie: {kcal_100g},\nLien: {link}\n".format(**row))
 
                                         add_sub = 1
 
                                         while add_sub:
                                             try:
-                                                prod_sub = int(input("Veuillez entrer l'ID du substitut que vous souhaitez: "))
+                                                prod_sub = int(input("Veuillez entrer l'ID du substitut"
+                                                                     " que vous souhaitez: "))
+
+                                                # And finally the req that add the substitute into the database
                                                 cursor.execute("UPDATE usr_products"
-                                                               " SET prod_substitute_id = %s WHERE prod_id = %s", (prod_sub,
-                                                                                                               product_id,))  # And finally the req that add the substitute into the database
+                                                               " SET prod_substitute_id = %s WHERE prod_id = %s",
+                                                               (prod_sub, product_id,))
                                                 cnx.commit()
                                                 another_prime_menu = 0
                                                 add_sub = 0
@@ -154,7 +170,7 @@ class Console:  # Main class of the program
                                                 print("\nLes lettres ne sont pas autorisées !")
 
                                 except ValueError:
-                                     print("\nLes lettres ne sont pas autorisées")
+                                    print("\nLes lettres ne sont pas autorisées")
 
                         except ValueError:
                             print("\nJ'ai dit que des chiffres !")
@@ -179,13 +195,16 @@ class Console:  # Main class of the program
 
         while find_it:
 
-            aliment = (input("\nVeuillez entrer le nom d'un aliment que vous recherchez:"))  # The user enter the name of the product that he is searching
+            # The user enter the name of the product that he is searching
+            aliment = (input("\nVeuillez entrer le nom d'un aliment que vous recherchez:"))
 
+            # SQL req that check every products by name that user entered, using operator "LIKE"
             cursor.execute("SELECT * FROM products"
-                    " WHERE name LIKE %(p)s ORDER by grade ASC",  {"p": "%{}%".format(aliment.replace(' ', '%'))})  # SQL req that check every products by name that user entered, using operator "LIKE"
+                           " WHERE name LIKE %(p)s ORDER by grade ASC", {"p": "%{}%".format(aliment.replace(' ', '%'))})
 
             for row in cursor:
-                print("Produit: {name}, Note: {grade}, ID: {prod_id}, Lien: {link}".format(**row))  # Then print the results
+                # Then print the results
+                print("Produit: {name}, Note: {grade}, ID: {prod_id}, Lien: {link}".format(**row))
                 find_it = 0
         time.sleep(0.5)
         self.menu_find(cursor)
@@ -202,7 +221,9 @@ class Console:  # Main class of the program
 
                 choice_menu_find = int(input("\nQue souhaitez vous faire ?\n"
                                              "Aller au menu des catégories (1)\nRetour au menu principal (2)\n"
-                                             "Chercher un autre aliment (3)\nEnregistrer un aliment dans la BDD (4)\nTrouver un substitut (5)"))
+                                             "Chercher un autre aliment (3)\nEnregistrer un aliment dans la BDD (4)"
+                                             "\nTrouver un substitut (5)"))
+
                 if choice_menu_find == 1:
                     self.catlist(cursor)
                     time.sleep(0.5)
@@ -222,6 +243,10 @@ class Console:  # Main class of the program
                         product_id = int(input("\nVeuillez entrer l'ID du produit que vous souhaitez enregistrer: "))
 
                         print("Enregistrement en cours...")
+
+                        ''' SQL req which inject the product ID contained into "product_id" var
+                         into the usr_products table '''
+
                         cursor.execute("INSERT INTO usr_products (prod_id) VALUES (%s)"
                                        " ON DUPLICATE KEY UPDATE prod_id = prod_id", (product_id, ))
                         cnx.commit()
@@ -252,28 +277,36 @@ class Console:  # Main class of the program
         while find_substitute_while:
 
             try:
-                substitut = int(input("\nVeuillez entrer l'ID du produit que vous souhaitez substituer: "))  # The user need to enter the product's ID of the one he want to substitute
+                # The user need to enter the product's ID of the one he want to substitute
+                substitut = int(input("\nVeuillez entrer l'ID du produit que vous souhaitez substituer: "))
 
-                cursor.execute("SELECT cat_id, grade, kcal_100g FROM products WHERE prod_id = %s", (substitut, ))  # Select the category's ID, grade and the energy per 100g as criteria to find substitute
+                # Select the category's ID, grade and the energy per 100g as criteria to find substitute
+                cursor.execute("SELECT cat_id, grade, kcal_100g FROM products WHERE prod_id = %s", (substitut, ))
 
                 for row in cursor:
 
+                    # Then compare every product of the same category with the grade and the energy per 100g
                     cursor.execute("SELECT * FROM products"
                                    " WHERE cat_id = {cat_id} AND grade <= '{grade}' AND kcal_100g <= {kcal_100g}"
-                                   " ORDER by grade ASC".format(**row))  # Then compare every product of the same category with the grade and the energy per 100g
+                                   " ORDER by grade ASC".format(**row))
 
                     for row in cursor:
 
                         print("\nID: {prod_id},\nNom: {name},\nDesc: {description},\nNote: {grade},\nEnergie:"
                               " {kcal_100g},\nLien: {link}\n".format(**row))  # And display them
 
-
-
-                    cursor.execute("SELECT prod_id, name FROM products WHERE prod_id = %s", (substitut, ))  # Another SQL req to display the product id and the name of the product...
+                    # Another SQL req to display the product id and the name of the product...
+                    cursor.execute("SELECT prod_id, name FROM products WHERE prod_id = %s", (substitut, ))
 
                     for row in cursor:
-                        which_one = int(input("Entrez l'ID du produit substituant: ({prod_id}) {name} ?".format(**row)))  # ...Here in case of the user forgot for what he asking a substitute
-                        cursor.execute("UPDATE usr_products SET prod_id = %s, prod_substitute_id = %s WHERE prod_id = %s", (substitut, which_one, substitut, ))  # Then update the line whith the substitute ID where the product ID is
+
+                        # ...Here in case of the user forgot for what he asking a substitute ({prod_id) {name})
+                        which_one = int(input("Entrez l'ID du produit substituant: ({prod_id}) {name} ?".format(**row)))
+
+                        # Then update the line whith the substitute ID where the product ID is
+                        cursor.execute("UPDATE usr_products"
+                                       " SET prod_id = %s, prod_substitute_id = %s WHERE prod_id = %s",
+                                       (substitut, which_one, substitut, ))
                         cnx.commit()
                         self.menu()
                         find_substitute_while = 0
@@ -328,8 +361,8 @@ class Console:  # Main class of the program
         cnx = mysql.connector.connect(**config)
         cursor = cnx.cursor(dictionary=True)
 
-        ''' This cursor.execute grab every informations about products and their substitutes. since the products id and the
-            substitute id exist in the same table, the program need to create alias from tables and rows to display
+        ''' This cursor.execute grab every informations about products and their substitutes. since the products id and
+            the substitute id exist in the same table, the program need to create alias from tables and rows to display
             everything'''
 
         cursor.execute("SELECT p.name AS prod_name, p.grade AS prod_grade, p.store AS prod_store, "
@@ -340,15 +373,15 @@ class Console:  # Main class of the program
                        "FROM usr_products up INNER JOIN products p ON up.prod_id = p.prod_id "
                        "LEFT JOIN products sp ON up.prod_substitute_id = sp.prod_id")
 
-        for row in cursor:
-            print("\n                                   \033[4mPRODUIT\033[0m:")  # "\033[4m" display the word with underlines
+        for row in cursor:            # "\033[4m" display the word with underlines
+            print("\n                                   \033[4mPRODUIT\033[0m:")
 
             print("\n\033[4mID\033[0m: {produ_id},\n\033[4mNom\033[0m: {prod_name},\n\033[4mLien\033[0m: {prod_link},"
                   "\n\033[4mNote\033[0m: {prod_grade},\n\033[4mMagasin\033[0m: {prod_store},"
                   "\n\033[4mValeur energetique\033[0m: {prod_energy},\n\033[4mDescription\033[0m: {prod_desc}".format(**row))
 
-
-            if ("{sub_prod_id}".format(**row)) != "None":  # If there is not a substitute id for a given id product then don't print anything
+            # If there is not a substitute id for a given id product then don't print anything
+            if ("{sub_prod_id}".format(**row)) != "None":
 
                 print("\n                                   \033[4mSUBSTITUT DE\033[0m: {prod_name}".format(**row))
                 print("\n\033[4mID du substitut\033[0m: {sub_prod_id},\n\033[4mNom du substitut\033[0m: {sub_name},"
